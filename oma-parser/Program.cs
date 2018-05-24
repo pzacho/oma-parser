af431ef878b28fe49e54be8c5e06bfacaedc41f1
Per Zacho <per.zacho@grandvision.com>
2018-05-24 09:51:43 +0200

    Add project files.

---
+++ oma-parser/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace oma_parser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
